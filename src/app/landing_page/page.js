112b479bc4637cff6e8689082dd6c750f2d6ff87
abdul-zacky <zacxvan@gmail.com>
2024-12-09 20:20:05 +0700

    first commit

--- src/app/landing_page/page.js
+++ src/app/landing_page/page.js
@@ -1,9 +1,9 @@
 "use client";
 
-import { useRouter } from 'next/navigation';
+// import { useRouter } from 'next/navigation';
 
 export default function LandingPage() {
-  const router = useRouter();
+  // const router = useRouter();
 
   return (
     <div className="flex items-center justify-center min-h-screen bg-gray-100">
